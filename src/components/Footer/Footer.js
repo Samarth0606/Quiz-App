706942342618a9033d015fc03fc067a84d19f620
samarth vohra <samarthvohra.india@gmail.com>
2022-01-21 04:29:38 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+      <div style={{textAlign:"center" , marginBottom:10}}>
+          Made with 💖by {" "}
+          <a href="https://www.instagram.com/vohra_samarthh/" style={{cursor:"pointer"}}>
+              Samarth Vohra
+          </a>
+
+      </div>
+  );
+};
+
+export default Footer;
